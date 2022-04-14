10ecb7ed015608370e5f3fc536b15d9fcc5c18a0
raduliano <radulian@me.com>
2022-04-14 22:53:04 +0300

    -create the ui of the app

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -37,7 +37,7 @@ struct ContentView: View {
                 }
                 
                 HStack {
-                    
+                //weather for next days
                 }
                           
             }
